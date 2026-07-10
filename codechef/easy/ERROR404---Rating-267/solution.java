befd45ff70b6f16f637509d0096377e8d0c04b0a
kancharla varshni satya hanu sai lasya <153889020+varshininaidukancharla@users.noreply.github.com>
2026-07-10 08:50:30 +0530

    ✅ ERROR404 - Rating 267 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-07-10

---
+++ codechef/easy/ERROR404---Rating-267/solution.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int n= sc.nextInt();
+		if(n==404){
+		    System.out.println("NOT FOUND");
+		}
+		else{
+		    System.out.println("FOUND");
+		}
+
+	}
+}
