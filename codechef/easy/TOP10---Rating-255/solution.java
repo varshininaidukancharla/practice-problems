133d95e8d87e7cb77cf1a47221af2b789c35ad4a
kancharla varshni satya hanu sai lasya <153889020+varshininaidukancharla@users.noreply.github.com>
2026-07-10 08:39:19 +0530

    ✅ TOP10 - Rating 255 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-07-10

---
+++ codechef/easy/TOP10---Rating-255/solution.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		for(int i=0;i<t;i++){
+		int x = sc.nextInt();
+		
+		if(x<=10){
+		    System.out.println("YES");
+		}
+		else{
+		    System.out.println("NO");
+		}
+}
+	}
+}
