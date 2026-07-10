81d51012c9d0c36c2a2222b847c4f4dfadff1a7c
kancharla varshni satya hanu sai lasya <153889020+varshininaidukancharla@users.noreply.github.com>
2026-07-10 08:43:25 +0530

    ✅ LUDO - Rating 260 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-07-10

---
+++ codechef/easy/LUDO---Rating-260/solution.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int t=sc.nextInt();
+		for(int i=0;i<t;i++){
+		    int n = sc.nextInt();
+		    if(n==6){
+		        System.out.println("YES");
+		    }
+		    else{
+		        System.out.println("NO");
+		    }
+		}
+
+	}
+}
