ab5ebe364c186e65908834014a16a0099e1c451b
kancharla varshni satya hanu sai lasya <153889020+varshininaidukancharla@users.noreply.github.com>
2026-07-10 08:30:10 +0530

    ✅ LUCKYSEVEN - Rating 213 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-07-10

---
+++ codechef/easy/LUCKYSEVEN---Rating-213/solution.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		String s = sc.nextLine();
+		System.out.println(s.charAt(6));
+	
+
+	}
+}
