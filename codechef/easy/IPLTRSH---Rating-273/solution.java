96cd370e3bdea2746b21052cd9cdc47f9b392cb3
kancharla varshni satya hanu sai lasya <153889020+varshininaidukancharla@users.noreply.github.com>
2026-07-10 11:40:33 +0530

    ✅ IPLTRSH - Rating 273 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-07-10

---
+++ codechef/easy/IPLTRSH---Rating-273/solution.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		for(int i=0;i<t;i++){
+		    int a =sc.nextInt();
+		    int b=sc.nextInt();
+		    if(a>=b){
+		        System.out.println(a-b);
+		    }
+		    else{
+		        System.out.println(0);
+		    }
+		}
+
+	}
+}
