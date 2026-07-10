55ade76fa300f70687b1d795a416bd877a80d5e7
kancharla varshni satya hanu sai lasya <153889020+varshininaidukancharla@users.noreply.github.com>
2026-07-10 08:26:13 +0530

    ✅ CWC23QUALIF - Rating 203 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-07-10

---
+++ codechef/easy/CWC23QUALIF---Rating-203/solution.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int n= sc.nextInt();
+		if(n>=12){
+		    System.out.println("Yes");
+		}
+		else{
+		    System.out.println("No");
+		}
+
+	}
+}
