cade64aca238ff9a0621915d48daa508ec17c3cc
kancharla varshni satya hanu sai lasya <153889020+varshininaidukancharla@users.noreply.github.com>
2026-07-10 08:55:04 +0530

    ✅ OFFBY1 - Rating 271 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-07-10

---
+++ codechef/easy/OFFBY1---Rating-271/solution.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int a= sc.nextInt();
+		int b=sc.nextInt();
+		int c= a+b;
+		int d= c*10;
+		System.out.println(d+1);
+
+	}
+}
