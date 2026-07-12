1dcb620f52cc1858be6a4457f63fde06a136dee4
kancharla varshni satya hanu sai lasya <153889020+varshininaidukancharla@users.noreply.github.com>
2026-07-12 12:11:49 +0530

    ✅ Odd or Even | Basic | Java | Runtime: N/A | Memory: N/A | 2026-07-12

---
+++ gfg/basic/odd-or-even3618/solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    static boolean isEven(int n) {
+        // code here
+        if(n%2!= 0){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+}
