e14843619dcafa79ed0c77010896ef98deba8b4e
kancharla varshni satya hanu sai lasya <153889020+varshininaidukancharla@users.noreply.github.com>
2026-07-12 10:23:23 +0530

    ✅ Repeated Character | Basic | Java | Runtime: N/A | Memory: N/A | 2026-07-12

---
+++ gfg/basic/repeated-character2058/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    char firstRep(String S) {
+        // your code here
+        for(int i=0;i<S.length();i++){
+            for(int j = i+1;j<S.length();j++){
+                if(S.charAt(i)==S.charAt(j)){
+                    return S.charAt(i);
+                }
+            }
+        }
+        return '#';
+    }
+}
