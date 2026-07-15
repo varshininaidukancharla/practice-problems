d01c5cf05b21acf4dedef5eb328cf8a9bd99bbfd
kancharla varshni satya hanu sai lasya <153889020+varshininaidukancharla@users.noreply.github.com>
2026-07-15 21:07:00 +0530

    ✅ Fighting the Darkness | Basic | Java | Runtime: N/A | Memory: N/A | 2026-07-15

---
+++ gfg/basic/fighting-the-darkness3949/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxDays(int arr[]) {
+        // code here
+        int max = arr[0];
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>max){
+                max = arr[i];
+            }
+        }
+        return max;
+        
+    }
+}
