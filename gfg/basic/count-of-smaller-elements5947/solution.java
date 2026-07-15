04bcb1ccaf01d2a63c82938f38ceba8e32dab245
kancharla varshni satya hanu sai lasya <153889020+varshininaidukancharla@users.noreply.github.com>
2026-07-15 19:10:39 +0530

    ✅ Count Smaller in an Array | Basic | Java | Runtime: N/A | Memory: N/A | 2026-07-15

---
+++ gfg/basic/count-of-smaller-elements5947/solution.java
@@ -0,0 +1,13 @@
+
+class Solution {
+    public int countOfElements(int x, List<Integer> arr) {
+        // code here
+        int count = 0;
+        for(int i=0;i<arr.size();i++){
+            if(arr.get(i) <= x){
+                count++;
+            }
+        }
+        return count;
+    }
+}
