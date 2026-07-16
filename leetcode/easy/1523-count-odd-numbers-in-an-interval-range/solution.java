d8497a46a73b0dccc5cac374e6af3fd0d65e79b4
kancharla varshni satya hanu sai lasya <153889020+varshininaidukancharla@users.noreply.github.com>
2026-07-16 22:16:35 +0530

    ✅ Count Odd Numbers in an Interval Range | Easy | Java | Runtime: 0 ms (100.00%) | Memory: 41.8 MB | 2026-07-16

--- leetcode/easy/1523-count-odd-numbers-in-an-interval-range/solution.java
+++ leetcode/easy/1523-count-odd-numbers-in-an-interval-range/solution.java
@@ -1,13 +1,11 @@
 class Solution {
     public int countOdds(int low, int high) {
-        int count =0;
-        for(int i=low;i<=high/2;i++){
-            if(i%2 !=0){
-                count++;
-            }
-            
+        int count = (high - low) / 2;
+
+        if (low % 2 != 0 || high % 2 != 0) {
+            count++;
         }
-        return count*2;
-        
+
+        return count;
     }
 }
