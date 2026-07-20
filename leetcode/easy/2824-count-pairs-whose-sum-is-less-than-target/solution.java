b71d2a4f51f5cf75ee19e870118a38e0e551a3b4
kancharla varshni satya hanu sai lasya <153889020+varshininaidukancharla@users.noreply.github.com>
2026-07-20 11:40:59 +0530

    ✅ Count Pairs Whose Sum is Less than Target | Easy | Java | Runtime: 3 ms (45.22%) | Memory: 44 MB | 2026-07-20

---
+++ leetcode/easy/2824-count-pairs-whose-sum-is-less-than-target/solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int countPairs(List<Integer> nums, int target) {
+        Collections.sort(nums);
+        int left = 0;
+        int right = nums.size()-1;
+        int count =0;
+        while(left<right){
+            int sum = nums.get(left)+nums.get(right);
+            if(sum<target){
+                count+=(right-left);
+                left++;
+            }
+            else{
+                right--;
+            }
+        }
+        return count;
+
+        
+    }
+}
