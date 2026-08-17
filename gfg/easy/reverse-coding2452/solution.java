57c0e6da9a9de5da769381a2ba6231fa68a21bf4
kancharla varshni satya hanu sai lasya <153889020+varshininaidukancharla@users.noreply.github.com>
2026-08-17 18:55:22 +0530

    ✅ Sum of Natural Numbers | Easy | Java | Runtime: N/A | Memory: N/A | 2026-08-17

---
+++ gfg/easy/reverse-coding2452/solution.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        // code here
+        System.out.print((n*(n+1))/2);
+    }
+}
