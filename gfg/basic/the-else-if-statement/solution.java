c017043d3e79fba833c2f66dd3e7b0a77fd03655
kancharla varshni satya hanu sai lasya <153889020+varshininaidukancharla@users.noreply.github.com>
2026-08-17 18:54:27 +0530

    ✅ The Else if Statement | Basic | Java | Runtime: N/A | Memory: N/A | 2026-08-17

---
+++ gfg/basic/the-else-if-statement/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public static void utility(int number) {
+        // Write the if, else if, else statements below
+        if(number>100){
+            System.out.println("Big");
+        }
+        else if(number<10){
+            System.out.println("Small");
+        }
+        else{
+            System.out.println("Number");
+        }
+    }
+}
