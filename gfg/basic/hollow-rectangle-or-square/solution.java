39210f27568adedb98dccfaff5801ca6978aa9a6
kancharla varshni satya hanu sai lasya <153889020+varshininaidukancharla@users.noreply.github.com>
2026-08-17 19:03:24 +0530

    ✅ Print Hollow Rectangle | Basic | Java | Runtime: N/A | Memory: N/A | 2026-08-17

---
+++ gfg/basic/hollow-rectangle-or-square/solution.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        // code here
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(i==0||j==0||i==n-1||j==m-1){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println("");
+        }
+    }
+}
