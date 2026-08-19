6bee0c384e5feb008bc34b32117d949421bd287e
kancharla varshni satya hanu sai lasya <153889020+varshininaidukancharla@users.noreply.github.com>
2026-08-19 10:30:49 +0530

    ✅ Count Zeros | Easy | Java | Runtime: N/A | Memory: N/A | 2026-08-19

---
+++ gfg/easy/count-the-zeros2550/solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    int countZeroes(int[] arr) {
+        // code here
+        int count=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==0){
+                count++;
+            }
+        }
+        return count;
+    }
+}
