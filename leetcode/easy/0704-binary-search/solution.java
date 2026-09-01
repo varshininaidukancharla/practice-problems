e222721bddaefd85744972308a5f857db4f3b82b
kancharla varshni satya hanu sai lasya <153889020+varshininaidukancharla@users.noreply.github.com>
2026-09-01 16:43:38 +0530

    ✅ Binary Search | Easy | Java | Runtime: 0 ms (100.00%) | Memory: 47.8 MB | 2026-09-01

--- leetcode/easy/0704-binary-search/solution.java
+++ leetcode/easy/0704-binary-search/solution.java
@@ -1,22 +1,22 @@
 class Solution {
-    public int search(int[] nums,int target) {
+    public int search(int[] nums, int target) {
+        int n= nums.length;
         int l=0;
-        int r=nums.length-1;
-
-        while(l<=r){
-            int mid=l+(r-l)/2;
-
-            if(nums[mid]==target){
+        int h=nums.length-1;
+        
+        while(l<=h){
+            int mid=l+(h-l)/2;
+            if(nums[mid]== target){
                 return mid;
             }
-            else if(nums[mid]<target){
+            if(nums[mid]<target){
                 l=mid+1;
             }
             else{
-                r=mid-1;
+                h=mid-1;
             }
         }
-
         return -1;
+        
     }
 }
